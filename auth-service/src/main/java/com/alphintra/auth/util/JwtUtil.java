02ac17f01d22baf1ee0454d75a0c1d1e2a485cd9
daninithi <140165739+daninithi@users.noreply.github.com>
2026-03-06 22:07:54 +0530

    refactor: reorganize import statements in JwtUtil for clarity

--- auth-service/src/main/java/com/alphintra/auth/util/JwtUtil.java
+++ auth-service/src/main/java/com/alphintra/auth/util/JwtUtil.java
@@ -1,17 +1,18 @@
 package com.alphintra.auth.util;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class JwtUtil {
